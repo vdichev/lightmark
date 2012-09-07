2d70ef91749bb153272e6d4268b5e23d275513bf
Vassil Dichev <vdichev@gmail.com>
2012-09-07 09:32:00 +0300

    Refactor tests

--- src/test/scala/lightmark/reStructuredTextParserSpec.scala
+++ src/test/scala/lightmark/reStructuredTextParserSpec.scala
@@ -12,9 +12,7 @@ class reStructuredTextParserSpec extends Specification {
       title("""|====
                |test
                |====""".stripMargin
-      ) must beLike {
-        case Success(Section("test", 1), _) => true
-      }
+      ).get must beEqualTo(Section("test", 1))
     }
 
     "parse inset markup" in {
@@ -22,9 +20,7 @@ class reStructuredTextParserSpec extends Specification {
       title("""|=====
                | test
                |=====""".stripMargin
-      ) must beLike {
-        case Success(Section("test", 1), _) => true
-      }
+      ).get must beEqualTo(Section("test", 1))
     }
 
     "fail shorter separator" in {
@@ -73,9 +69,7 @@ class reStructuredTextParserSpec extends Specification {
       new rstParser().
       title("""|test
                |====""".stripMargin
-      ) must beLike {
-        case Success(Section("test", 1), _) => true
-      }
+      ).get must beEqualTo(Section("test", 1))
     }
 
     "fail inset markup" in {
@@ -118,12 +112,11 @@ class reStructuredTextParserSpec extends Specification {
              |=====
              |test4
              |+++++""".stripMargin
-      ) must beLike {
-        case Success(List(Section("test", 1),
-                          Section("test2", 2),
-                          Section("test3", 1),
-                          Section("test4", 3)), _) => true
-      }
+      ).get must beEqualTo(List(
+        Section("test", 1),
+        Section("test2", 2),
+        Section("test3", 1),
+        Section("test4", 3)))
     }
     "match a section with paragraphs" in {
       new rstParser().
@@ -134,12 +127,10 @@ class reStructuredTextParserSpec extends Specification {
              |more text
              |
              |""".stripMargin
-      ) must beLike {
-        case Success(List(
-          Section("test", 1),
-          FormattedParagraph(List(PlainText("text"))),
-          FormattedParagraph(List(PlainText("more text")))), _) => true
-      }
+      ).get must beEqualTo(List(
+        Section("test", 1),
+        FormattedParagraph(List(PlainText("text"))),
+        FormattedParagraph(List(PlainText("more text")))))
     }
     "match multiple sections with paragraphs" in {
       new rstParser().
@@ -157,49 +148,37 @@ class reStructuredTextParserSpec extends Specification {
              |sec3
              |====
              |another level 1 section""".stripMargin
-      ) must beLike {
-        case Success(List(
-          Section("sec1", 1),
-          FormattedParagraph(List(PlainText("text"))),
-          FormattedParagraph(List(PlainText("more text"))),
-          Section("sec2", 2),
-          FormattedParagraph(List(PlainText("subsection text"))),
-          Section("sec3", 1),
-          FormattedParagraph(List(PlainText("another level 1 section")))), _) => true
-      }
+      ).get must beEqualTo(List(
+        Section("sec1", 1),
+        FormattedParagraph(List(PlainText("text"))),
+        FormattedParagraph(List(PlainText("more text"))),
+        Section("sec2", 2),
+        FormattedParagraph(List(PlainText("subsection text"))),
+        Section("sec3", 1),
+        FormattedParagraph(List(PlainText("another level 1 section")))))
     }
   }
 
   "paragraphs" should {
     "end in a blank line" in {
-      paragraph(0)("text\n\n") must beLike {
-        case Success(Paragraph("text"), _) => true
-      }
+      paragraph(0)("text\n\n").get must beEqualTo(Paragraph("text"))
     }
     "end in a line with just spaces" in {
-      paragraph(0)("text\n  \n") must beLike {
-        case Success(Paragraph("text"), _) => true
-      }
+      paragraph(0)("text\n  \n").get must beEqualTo(Paragraph("text"))
     }
     "end at the end of the text" in {
-      paragraph(0)("text") must beLike {
-        case Success(Paragraph("text"), _) => true
-      }
+      paragraph(0)("text").get must beEqualTo(Paragraph("text"))
     }
     "work for expected indent for consecutive lines" in {
       paragraph(2)("""|line1
                       |  line2""".stripMargin
-      ) must beLike {
-        case Success(Paragraph("line1 line2"), _) => true
-      }
+      ).get must beEqualTo(Paragraph("line1 line2"))
     }
     "stop parsing for different indent for consecutive lines" in {
       paragraph(2)("""|line1
                       |   line2
                       |""".stripMargin
-      ) must beLike {
-        case Success(Paragraph("line1"), _) => true
-      }
+      ).get must beEqualTo(Paragraph("line1"))
     }
   }
 
@@ -207,31 +186,25 @@ class reStructuredTextParserSpec extends Specification {
     "parse items on consecutive lines" in {
       bulletList(0)("""|* item1
                        |* item2""".stripMargin
-      ) must beLike {
-        case Success(BulletList(2, List(
-          BulletItem(List(FormattedParagraph(List(PlainText("item1"))))),
-          BulletItem(List(FormattedParagraph(List(PlainText("item2"))))))), _) => true
-      }
+      ).get must beEqualTo(BulletList(2, List(
+        BulletItem(List(FormattedParagraph(List(PlainText("item1"))))),
+        BulletItem(List(FormattedParagraph(List(PlainText("item2"))))))))
     }
 
     "parse items separated by blank lines" in {
       bulletList(0)("""|* item1
                        |
                        |* item2""".stripMargin
-      ) must beLike {
-        case Success(BulletList(2, List(
-          BulletItem(List(FormattedParagraph(List(PlainText("item1"))))),
-          BulletItem(List(FormattedParagraph(List(PlainText("item2"))))))), _) => true
-      }
+      ).get must beEqualTo(BulletList(2, List(
+        BulletItem(List(FormattedParagraph(List(PlainText("item1"))))),
+        BulletItem(List(FormattedParagraph(List(PlainText("item2"))))))))
     }
 
     "parse items of multi-line paragraphs" in {
       bulletList(0)("""|* line1
                        |  line2""".stripMargin
-      ) must beLike {
-        case Success(BulletList(2, List(
-          BulletItem(List(FormattedParagraph(List(PlainText("line1 line2"))))))), _) => true
-      }
+      ).get must beEqualTo(BulletList(2, List(
+        BulletItem(List(FormattedParagraph(List(PlainText("line1 line2"))))))))
     }
 
     "parse items with multiple paragraphs" in {
@@ -239,21 +212,17 @@ class reStructuredTextParserSpec extends Specification {
                        |  paragraph 1 line 2
                        |
                        |  paragraph 2""".stripMargin
-      ) must beLike {
-        case Success(BulletList(2, List(BulletItem(List(
-          FormattedParagraph(List(PlainText("paragraph 1 line 1 paragraph 1 line 2"))),
-          FormattedParagraph(List(PlainText("paragraph 2"))))))), _) => true
-      }
+      ).get must beEqualTo(BulletList(2, List(BulletItem(List(
+        FormattedParagraph(List(PlainText("paragraph 1 line 1 paragraph 1 line 2"))),
+        FormattedParagraph(List(PlainText("paragraph 2"))))))))
     }
 
     "parse items containing other bullet lists" in {
       bulletList(0)("""|* - item1
                        |  - item2""".stripMargin
-      ) must beLike {
-        case Success(BulletList(2, List(BulletItem(List(BulletList(4, List(
-          BulletItem(List(FormattedParagraph(List(PlainText("item1"))))),
-          BulletItem(List(FormattedParagraph(List(PlainText("item2"))))))))))), _) => true
-      }
+      ).get must beEqualTo(BulletList(2, List(BulletItem(List(BulletList(4, List(
+        BulletItem(List(FormattedParagraph(List(PlainText("item1"))))),
+        BulletItem(List(FormattedParagraph(List(PlainText("item2"))))))))))))
     }
 
     "parse items of lists of multiple multi-line paragraphs" in {
@@ -268,120 +237,100 @@ class reStructuredTextParserSpec extends Specification {
                        |  list1 item1 paragraph
                        |
                        |* list1 item2""".stripMargin
-      ) must beLike {
-        case Success(
-          BulletList(2, List(
-            BulletItem(List(
-              FormattedParagraph(List(PlainText("list1 item1"))),
-              BulletList(4, List(
-                BulletItem(List(
-                  FormattedParagraph(List(PlainText("list2 item1 paragraph1 line1 list2 item1 paragraph1 line2"))),
-                  FormattedParagraph(List(PlainText("list2 item1 paragraph2")))
-                )),
-                BulletItem(List(
-                  FormattedParagraph(List(PlainText("list2 item2 paragraph1")))
-                ))
+      ).get must beEqualTo(
+        BulletList(2, List(
+          BulletItem(List(
+            FormattedParagraph(List(PlainText("list1 item1"))),
+            BulletList(4, List(
+              BulletItem(List(
+                FormattedParagraph(List(PlainText("list2 item1 paragraph1 line1 list2 item1 paragraph1 line2"))),
+                FormattedParagraph(List(PlainText("list2 item1 paragraph2")))
               )),
-              FormattedParagraph(List(PlainText("list1 item1 paragraph")))
+              BulletItem(List(
+                FormattedParagraph(List(PlainText("list2 item2 paragraph1")))
+              ))
             )),
-            BulletItem(List(
-              FormattedParagraph(List(PlainText("list1 item2")))
-            ))
-          )), _) => true
-      }
+            FormattedParagraph(List(PlainText("list1 item1 paragraph")))
+          )),
+          BulletItem(List(
+            FormattedParagraph(List(PlainText("list1 item2")))
+          ))
+        )))
     }
   }
   
   "inline markup" should {
     "parse simple emphasis" in {
-      emph("*some text* ") must beLike {
-        case Success(Emph("some text"), _) => true
-      }
+      emph("*some text* ").get must beEqualTo(Emph("some text"))
     }
     
     "parse combination of emphasis, text and strong emphasis" in {
-      par("*emph* then text and **strong** ") must beLike {
-        case Success(List(
-          Emph("emph"),
-          PlainText(" then text and "),
-          Strong("strong"),
-          PlainText(" ")
-        ), _) => true
-      }
+      par("*emph* then text and **strong** ").get must beEqualTo(List(
+        Emph("emph"),
+        PlainText(" then text and "),
+        Strong("strong"),
+        PlainText(" ")
+      ))
     }
     
     "parse text with a single asterisk as plain text" in {
-      par("an * asterisk ") must beLike {
-        case Success(List(PlainText("an * asterisk ")), _) => true
-      }
+      par("an * asterisk ").get must beEqualTo(List(PlainText("an * asterisk ")))
     }
     
     "parse text with no asterisks as plain text" in {
-      plainText("plain text ") must beLike {
-        case Success(PlainText("plain text "), _) => true
-      }
+      plainText("plain text ").get must beEqualTo(PlainText("plain text "))
     }
     
     "start-strings must not be preceded by a word character" in {
-      par("a*emph* ") must beLike {
-        case Success(List(PlainText("a*emph* ")), _) => true
-      }
+      par("a*emph* ").get must beEqualTo(List(PlainText("a*emph* ")))
     }
     
     "start-strings must not be followed by whitespace" in {
-      par("a * text* ") must beLike {
-        case Success(List(PlainText("a * text* ")), _) => true
-      }
+      par("a * text* ").get must beEqualTo(List(PlainText("a * text* ")))
     }
     
     "end-strings must not be preceded by whitespace" in {
-      par("*text * ") must beLike {
-        case Success(List(PlainText("*text * ")), _) => true
-      }
+      par("*text * ").get must beEqualTo(List(PlainText("*text * ")))
     }
     
     "there can be an asterisk inside an emphasis" in {
-      par("*begin * end* ") must beLike {
-        case Success(List(
-          Emph("begin * end"),
-          PlainText(" ")
-        ), _) => true
-      }
+      par("*begin * end* ").get must beEqualTo(List(
+        Emph("begin * end"),
+        PlainText(" ")
+      ))
     }
     
     "end-strings must not be followed by a word character" in {
-      par("*emph*x ") must beLike {
-        case Success(List(PlainText("*emph*x ")), _) => true
-      }
+      par("*emph*x ").get must beEqualTo(List(PlainText("*emph*x ")))
     }
   }
 
   "escapes" should {
     val format = formattedParagraph(0)
     "escaped start-string shouldn't start inline" in {
-      format("""\*emph*""").get must be_==(FormattedParagraph(List(
+      format("""\*emph*""").get must beEqualTo(FormattedParagraph(List(
         PlainText("*emph*"))))
     }
 
     "escaped end-string shouldn't end inline" in {
-      format("""*emph\* end*""").get must be_==(FormattedParagraph(List(
+      format("""*emph\* end*""").get must beEqualTo(FormattedParagraph(List(
         Emph("emph* end"),
         PlainText(""))))
     }
 
     "escape after end-string shouldn't end inline" in {
-      format("""*emph*\s""").get must be_==(FormattedParagraph(List(
+      format("""*emph*\s""").get must beEqualTo(FormattedParagraph(List(
         Emph("emph"),
         PlainText("s"))))
     }
 
     "escaped backslash shouldn't escape start-string" in {
-      format("""\\*emph*""").get must be_==(FormattedParagraph(List(
+      format("""\\*emph*""").get must beEqualTo(FormattedParagraph(List(
         PlainText("""\*emph*"""))))
     }
 
     "escaped backslash shouldn't escape end-string" in {
-      format("""*emph\\*""").get must be_==(FormattedParagraph(List(
+      format("""*emph\\*""").get must beEqualTo(FormattedParagraph(List(
         Emph("""emph\"""),
         PlainText(""))))
     }
